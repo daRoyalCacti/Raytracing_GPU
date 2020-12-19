bb571bee65438e8520347fe5cde21371dbed8e91
George George <srdrmrdave@gmail.com>
2020-12-19 17:48:49 +1000

    finished tree construction and added collision

--- bvh.h
+++ bvh.h
@@ -6,9 +6,12 @@
 #include "hittable_list.h"
 #include "box.h"
 
+#define newa
+
 struct node_info {
 	bool end;
 	int num, left, right, parent;
+	int* ids;	//ids of the objs that a node bounds
 };
 
 
@@ -17,22 +20,28 @@ int num_bvh_nodes(int n) {
 }
 
 
+struct hittable_id {
+	hittable obj;
+	int index;
+};
 
 
 //helper function to be used in merge sort
-__device__ hittable* merge(const hittable* A, int L1, int R1, int L2, int R2, int axis) {
+__device__ hittable_id* merge(const hittable_id* A, int L1, int R1, int L2, int R2, int axis) {
 	//A array to be sorted
 	//L1 the start of the first part
 	//R1 the end of the first part
 	//L2 the start of the second part
 	//R2 the end of the second part
 	int index = 0;
-	hittable* temp = new hittable[R1 - L1 + R2 - L2+1];
+#ifdef newa
+	hittable_id* temp = new hittable_id[R1 - L1 + R2 - L2+1];
+#endif
 	aabb box1, box2;
 	
 	while (L1 <= R1 && L2 <= R2) {
-		A[L1].bounding_box(0, 0, box1);
-		A[L2].bounding_box(0, 0, box2);
+		A[L1].obj.bounding_box(0, 0, box1);
+		A[L2].obj.bounding_box(0, 0, box2);
 		//if (A[L1] <= A[L2]) {
 		if (box1.min().e[axis] <= box2.min().e[axis]) {
 			temp[index] = A[L1];
@@ -62,15 +71,18 @@ __device__ hittable* merge(const hittable* A, int L1, int R1, int L2, int R2, in
 
 
 
-__device__ void merge_sort(const hittable* A, int n, hittable* O, int axis) {
+__device__ void merge_sort(const hittable* A, int n, int* O, int axis) {
 	//A input array
 	//n size of input array
 	//O output array
-	
+#ifdef newa	
+	hittable_id* Out = new hittable_id[n];
+#endif
 	//might need ===================================
-	/*for (int k = 0; k < n; k++) { 
-		O[k] = A[k];
-	}*/
+	for (int k = 0; k < n; k++) { 
+		Out[k].obj = A[k];
+		Out[k].index = k;
+	}
 	//=============================================
 
 
@@ -92,36 +104,45 @@ __device__ void merge_sort(const hittable* A, int n, hittable* O, int axis) {
 			if (R2 >= n)
 				R2 = n-1;
 
-			auto temp = merge(O, L1, R1, L2, R2, axis);
+			auto temp = merge(Out, L1, R1, L2, R2, axis);
 			
 			for (int j=0; j < R2-L1+1; j++) { 
 				//std::cout << "i+j " << i+j << std::endl;
-				O[i+j] = temp[j];
+				Out[i+j] = temp[j];
 			}
 
 			i += 2*len;
 		}
 		len *= 2;
 	}
+
+	for (int i = 0; i < n; i++) {
+		O[i] = Out[i].index;
+	}
 }
 
 
+
+
+
 struct bvh_node : public hittable {
-	hittable* hittables;
+	hittable* objs;	//the actual objects
 	__device__ bvh_node(hittable* hits, int num_obj, const float time0, const float time1, curandState *s);
 	node_info* info;
 	int n;	//number of objects associated to the tree
+	aabb* bounds;	//the bounding boxes for each node of the tree
+	int* obj_s[3];	//the sorted indices of the objects based on an axis
 
 	//__device__ bvh_node();
 	__device__ bvh_node(int num_obj);
 
-	__device__ int num_nodes(){
+	__device__ int num_nodes() const {
 		return static_cast<int>(powf(2, ceilf(log2f(n)) ) -1 +n);
 	}
 
 
 
-	__device__ int index_at(int row, int col) {
+	__device__ int index_at(int row, int col) const {
 		return powf(2,row) - 1 + col;	
 	}
 
@@ -136,6 +157,9 @@ struct bvh_node : public hittable {
 
 __device__ bvh_node::bvh_node(int num_obj) : n(num_obj) {
 	const int num_ne_rows = ceilf(log2f(n));
+#ifdef newa
+	info = new node_info[num_nodes()];
+#endif
 	
 	int index;
 	int k;
@@ -244,13 +268,168 @@ __device__ bvh_node::bvh_node(int num_obj) : n(num_obj) {
 
 
 __device__ bvh_node::bvh_node(hittable* hits, int  num_obj, const float time0, const float time1, curandState *s) : bvh_node(num_obj) {
+	objs = hits;	//not sure if will work
+	//first filling the sorted arrays
+#ifdef newa
+	obj_s[0] = new int[n];
+	obj_s[1] = new int[n];
+	obj_s[2] = new int[n];
+
+	merge_sort(objs, n, obj_s[0], 0);
+	merge_sort(objs, n, obj_s[1], 1);
+	merge_sort(objs, n, obj_s[2], 2);
+#endif
+
+	//the first node contains all objects
+#ifdef newa
+	info[0].ids = new int[n];
+#endif
+	for (int i = 0; i < n; i++)
+		info[0].ids[i] = i;
+
+	//filling the nodes with the info about the objects they bound
+	for (int node = 0; node < num_nodes() - n; node++) {	//iterating through all nodes less the last row (each node modifies the ids of its children)
+		int axis = random_int(s, 0, 2);			
+#ifdef newa
+		info[node].ids = new int[info[node].num];
+#endif
+		//setting the ids of the objects passed down to the children
+		
+		//first sorting the objects by the axis
+		//	iterating throuhg the sorted array of objects in their sorted order
+		//	the ids that appear first in the sorted array get added to the children nodes in order
+		//	thus splitting the objects between the children based on their ordering along a given axis
+		int counter = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < info[node].num; j++) {
+				if (obj_s[axis][i] == info[node].ids[j]) {
+					if (counter < info[info[node].left].num) {
+						info[info[node].left].ids[counter++] = info[node].ids[j];
+						break;
+					} else {
+						info[info[node].right].ids[counter++] = info[node].ids[j];
+						break;
+					}
+
+				}
+			}
+		}
+	}	
+	
+	//creating the bounding boxes
+#ifdef newa
+	bounds = new aabb[num_nodes() - n];
+#endif
+	
+	//first for the second last row
+	const int num_ne_rows = ceilf(log2f(n));
+	aabb temp_box1, temp_box2;
+	for (int node = index_at(num_ne_rows - 1, 0); node < index_at(num_ne_rows,0); node++) {
+		if (info[node].num == 1) {
+			objs[info[info[node].left].ids[0]].bounding_box(0, 0, bounds[node]);
+		} else {
+			objs[info[info[node].left].ids[0]].bounding_box(0,0, temp_box1);
+			objs[info[info[node].right].ids[1]].bounding_box(0,0, temp_box2);
+			bounds[node] = surrounding_box(temp_box1, temp_box2);
+		}
+	}
+
+	//for the rest of the rows
+	for (int node = index_at(num_ne_rows - 2, 0); node <= 0; node++) {	//running through the rest of the nodes backwards
+		bounds[node] = surrounding_box(bounds[info[node].left], bounds[info[node].right]);
+	}
 
 }
 
 __device__ bool bvh_node::hit(const ray& r, const float t_min, const float t_max, hit_record& rec, curandState* s) const {
+#ifdef newa
+	int* lr = new int[ceilf(log2f(n))];	//number of non end rows
+#else
+	int* lr;
+#endif
+
+	int row_cntr = 0;	//counter for the current row
+#ifdef newa
+	int* col_cntr = new int[ceilf(log2f(n))];	//counts the current column for each row
+#else
+	int* col_cntr;
+#endif
+	for (int i = 0; i < sizeof(col_cntr)/sizeof(int); i++) {
+		col_cntr[i] = 0;
+		lr[i] = 0;
+	}
+
+	bool end = false;
+
+	int current_index;
+
+	float current_hit_time = infinity; 
+	hit_record temp_rec;
+	while (!end) {
+		current_index = index_at(row_cntr, col_cntr[row_cntr]);
+
+		if (info[current_index].end) {	//if at an end node
+			if (info[info[current_index].parent].num == 1) {
+				if (objs[info[current_index].ids[0]].hit(r, t_min, t_max, temp_rec, s) ) {	//if an end node is hit
+					if (temp_rec.t < current_hit_time) {
+						current_hit_time = temp_rec.t;
+						rec = temp_rec;
+					} else {	//there was a hit but it was after the current hit
+						row_cntr--;		//move up 
+						col_cntr[row_cntr]++;	//and across
+					}
+				} else {	//there was no hit
+					row_cntr--;		//move up
+					col_cntr[row_cntr]++;	//and across
+				}
+			} else {	//currently checking an end node that has 2 objects attacted to the parent node
+				if (objs[info[current_index].ids[0]].hit(r, t_min, t_max, temp_rec, s) ) {
+					if (temp_rec.t < current_hit_time) {
+						current_hit_time = temp_rec.t;
+						rec = temp_rec;
+					} 
+				}
+				if (objs[info[current_index].ids[1]].hit(r, t_min, t_max, temp_rec, s) ) {
+					if (temp_rec.t < current_hit_time) {
+						current_hit_time = temp_rec.t;
+						rec = temp_rec;
+					} else {
+						row_cntr--;		//move up
+						col_cntr[row_cntr]++;	//and across
+					}
+				}
+
+			}
+		} else {	//not an end node
+			if (bounds[current_index].hit(r, t_min, t_max) ) {	//ray hit a bounding box
+				row_cntr--;	//move down a row
+				col_cntr[row_cntr] = info[current_index].left;
+			} else { 	//ray did not hit a bounding box
+				if (lr[row_cntr] == 0) {	//at the first child node
+					lr[row_cntr] = 1;	
+					col_cntr[row_cntr]++;	//move to the second child node
+				} else {	//at the second child node
+					lr[row_cntr] = 0;	//resetting the variable
+					while (lr[--row_cntr] == 1) {//keep moving up until get back to a left node
+						lr[row_cntr] = 0;	//resetting the variable
+					}						
+					col_cntr[row_cntr]++;
+				}
+			}
+		}
+
+
+		if (col_cntr[row_cntr] > powf(2, row_cntr)) {
+			end = true;
+		}
+	}
+
+
+
 	return false;
 }
 __device__ bool bvh_node::bounding_box(const float time0, const float time1, aabb& output_box) const {
-	return false;
+	output_box = bounds[0];
+	return true;
 }
 
